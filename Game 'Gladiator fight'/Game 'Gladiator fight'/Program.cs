a2e87272df5e746485894f213b6eb89ec5baa51c
Nikorav <82765596+Nikorav@users.noreply.github.com>
2021-09-13 21:09:00 +0300

    init commit

---
+++ Game 'Gladiator fight'/Game 'Gladiator fight'/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Game__Gladiator_fight_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            float healthOne = rand.Next(90,100);
+            int damageOne = rand.Next(5, 25);
+            int armorOne = rand.Next(25, 65);
+            float healthTwo = rand.Next(80, 150);
+            int damageTwo = rand.Next(20, 40);
+            int armorTwo = rand.Next(65, 100);
+
+            Console.WriteLine($"Гладиатор 1 - {healthOne} хп, {damageOne} максимальный урон, {armorOne} броня");
+            Console.WriteLine($"Гладиатор 2 - {healthTwo} хп, {damageTwo} максимальный урон, {armorTwo} броня");
+
+            while (healthOne > 0 && healthTwo > 0)
+            {
+                healthOne -= Convert.ToSingle(rand.Next(0, damageTwo)) / 100 * armorOne;
+                healthTwo -= Convert.ToSingle(rand.Next(0, damageOne)) / 100 * armorTwo;
+                Console.WriteLine($"Гладиатор 1 - {healthOne}");
+                Console.WriteLine($"Гладиатор 2 - {healthTwo}");
+            }
+
+            if(healthOne <= 0 && healthTwo <= 0)
+            {
+                Console.WriteLine("Ничья, Оба мертвы.");          
+            } 
+            else if(healthOne <= 0)
+            {
+                Console.WriteLine("Глабиатор 1 пал в бою.");
+            }
+            else if(healthTwo <= 0)
+            {
+                Console.WriteLine("Глабиатор 2 пал в бою.");
+            }
+        }
+    }
+}
---
+++ Game 'Guess the number'/Game 'Guess the number'/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Game__Guess_the_number_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, lower, larger;
+            int tryCount = 5;
+            Random rand = new Random();
+            int userInput;
+
+            number = rand.Next(0, 101);
+            lower = rand.Next(number - 10, number);
+            larger = rand.Next(number + 1, number + 10);
+
+            Console.WriteLine($"Мы загадали число от 0 до 100, оно больше чем {lower}, но меньше чем {larger}");
+            Console.WriteLine("Что это за число");
+
+            while(tryCount-- > 0)
+            {
+                Console.Write("Это число:");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if(userInput == number)
+                {
+                    Console.WriteLine($"Вы правы! Это было число {number}");
+                    break;
+                } 
+                else
+                {
+                    Console.WriteLine("Не верно! Попробуйте ещё раз.");
+                }
+            }
+            
+            if(tryCount <= 0)
+            {
+                Console.WriteLine($"Увы, вы проиграли! Это было число {number}");
+            }
+        }
+    }
+}
